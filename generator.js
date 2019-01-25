fb078250661ee9bd9a7e8721bbd42469b1e0f53d
hz <530765937@qq.com>
2019-01-25 16:40:49 +0800

    fet

--- generator.js
+++ generator.js
@@ -1,3 +1,4 @@
+const cb = require('./filemid')
 module.exports = (api, options, rootOptions) => {
   // 修改 `package.json` 里的字段
   api.extendPackage({
@@ -8,7 +9,7 @@ module.exports = (api, options, rootOptions) => {
 
   // 复制并用 ejs 渲染 `./template` 内所有的文件
   api.render('./template')
-
+  api.postProcessFiles(cb)
   if (options.foo) {
     // 有条件地生成文件
   }
