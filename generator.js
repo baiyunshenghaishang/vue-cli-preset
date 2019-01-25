0574e8dbeabcfff4b5f33f5c1757f9cd40f8af74
hz <530765937@qq.com>
2019-01-25 16:03:36 +0800

    feat

--- generator.js
+++ generator.js
@@ -1,8 +1,8 @@
 module.exports = (api, options, rootOptions) => {
   // 修改 `package.json` 里的字段
   api.extendPackage({
-    scripts: {
-      test: 'vue-cli-service test'
+    devDependencies: {
+      vconsole: '^3.2.0'
     }
   })
 
