5be318b500ae30bcd42e70066f116083c2f432cb
hz <530765937@qq.com>
2019-01-26 15:12:39 +0800

    fix

--- changebrowserslist.js
+++ changebrowserslist.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 const path = require('path')
+const configUrl = path.resolve('./template/.browserslistrc')
 let browserslistconfig = fs.readFileSync(path.resolve('./template/.browserslistrc'), 'utf8')
-
+console.log('changebrowser...-')
+console.log(configUrl)
 module.exports = function(file, ejs) {
   file['.browserslistrc'] = browserslistconfig
 }
--- generator.js
+++ generator.js
@@ -1,4 +1,5 @@
 const changebrowserslistrc = require('./changebrowserslist.js')
+const path = require('path')
 module.exports = (api, options, rootOptions) => {
   // 修改 `package.json` 里的字段
   api.extendPackage({
@@ -6,7 +7,7 @@ module.exports = (api, options, rootOptions) => {
       vconsole: '^3.2.0'
     }
   })
-
+console.log(path.resolve('./browserlistsrc'));
   // 复制并用 ejs 渲染 `./template` 内所有的文件
   api.render('./template')
   api.postProcessFiles(changebrowserslistrc)
