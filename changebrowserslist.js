2316e1b7cf102fe80fbab0039d93d78f4f6c2e31
hz <530765937@qq.com>
2019-01-26 15:14:13 +0800

    test

--- changebrowserslist.js
+++ changebrowserslist.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 const path = require('path')
 const configUrl = path.resolve('./template/.browserslistrc')
-let browserslistconfig = fs.readFileSync(path.resolve('./template/.browserslistrc'), 'utf8')
 console.log('changebrowser...-')
 console.log(configUrl)
+let browserslistconfig = fs.readFileSync(path.resolve('./template/.browserslistrc'), 'utf8')
 module.exports = function(file, ejs) {
   file['.browserslistrc'] = browserslistconfig
 }
