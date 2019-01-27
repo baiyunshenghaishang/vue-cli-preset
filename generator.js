ebb68613330cc59cb8960f96b201c70a37eb4862
hz <530765937@qq.com>
2019-01-27 10:31:01 +0800

    fix

--- generator.js
+++ generator.js
@@ -10,6 +10,9 @@ module.exports = (api, options, rootOptions) => {
     }
   })
   // 复制并用 ejs 渲染 `./template` 内所有的文件
+  // api.render({
+  //   './.prettierrc':'./template/_prettierrc',
+  // })
   api.render('./template/_prettierrc')
   api.render('./template/vue.config.js')
 
