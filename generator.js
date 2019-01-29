99ec057d3d3d642d757c0bd12eddfd81f3920ef5
hz <530765937@qq.com>
2019-01-29 11:21:03 +0800

    feat 增加lib入口main字段

--- generator.js
+++ generator.js
@@ -1,24 +1,25 @@
 const changebrowserslistrc = require('./changebrowserslist.js')
 module.exports = (api, options, rootOptions) => {
-  // 修改 `package.json` 里的字段
-  api.extendPackage({
-    scripts: {
-      start: 'npm run serve'
-    },
-    devDependencies: {
-      vconsole: '^3.2.0'
-    }
-  })
-  // 复制并用 ejs 渲染 `./template` 内所有的文件
-  api.render('./root')
-  // api.postProcessFiles(changebrowserslistrc)
-  if (options.lib) {
-    // 有条件地生成文件
-    api.render('./lib')
+    // 修改 `package.json` 里的字段
     api.extendPackage({
-      scripts: {
-        build: `vue-cli-service build --target lib --name ${rootOptions.projectName} ./source/index.js`
-      }
+        scripts: {
+            start: 'npm run serve'
+        },
+        devDependencies: {
+            vconsole: '^3.2.0'
+        }
     })
-  }
+    // 复制并用 ejs 渲染 `./template` 内所有的文件
+    api.render('./root')
+    // api.postProcessFiles(changebrowserslistrc)
+    if (options.lib) {
+        // 有条件地生成文件
+        api.render('./lib')
+        api.extendPackage({
+            scripts: {
+                build: `vue-cli-service build --target lib --name ${rootOptions.projectName} ./source/index.js`
+            },
+            main: `./dist/${rootOptions.projectName}.umd.min.js`
+        })
+    }
 }
